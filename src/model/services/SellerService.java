51dc2cd397e38cd43c7626719f760c366102bd14
Lucas Tavares <t.luuccas@gmail.com>
2020-06-22 16:15:21 -0300

    SellerList

---
+++ src/model/services/SellerService.java
@@ -0,0 +1,31 @@
+package model.services;
+
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerService {
+
+	//Busca no Mysql a tabela
+	private SellerDao dao = DaoFactory.createSellerDao();
+	
+	public List<Seller> findAll(){
+		return dao.findAll();
+	}
+	
+	//Método para inserir e atualizar departamento
+	public void saveOrUpdate(Seller obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
+	
+	public void remove(Seller obj) {
+		dao.deleteById(obj.getId());
+	}
+}
